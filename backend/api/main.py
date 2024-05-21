a7eabdfbc6a3a009bc647e67656d4820b5ae96b4
Roy Bahian <roybay04@gmail.com>
2024-05-21 09:42:44 -0400

    chore: add customization to fastapi swager

--- backend/api/main.py
+++ backend/api/main.py
@@ -4,8 +4,28 @@ from pydantic import BaseModel
 from pony.orm import db_session, select
 from api.orm import connect_db, Role
 
+tags_metadata = [
+    {
+        "name": "roles",
+        "description": "Operations with ROLES. The **login** logic is also here.",
+    },
+]
 
-app = FastAPI()
+app = FastAPI(
+    title="HyeTheater API",
+    description="""
+    This is a custom description for my API.
+
+    ## Features
+
+    * **Feature 1**: Description of feature 1.
+    * **Feature 2**: Description of feature 2.
+    """,
+    version="1.0.0",
+    openapi_tags=tags_metadata,
+    swagger_ui_parameters={"defaultModelsExpandDepth": -1}
+    # Hide Schema
+)
 connect_db()
 
 
@@ -13,8 +33,9 @@ class GetRoleName(BaseModel):
     roles: List[str]
 
 
-@app.get("/api/v1/roles", response_model=GetRoleName)
+@app.get("/api/v1/roles", tags=["roles"], response_model=GetRoleName)
 @db_session
 def get_list_role_names():
     roles = select(r.name for r in Role)[:]
     return {"roles": list(roles)}
+
